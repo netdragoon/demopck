25cc22ec25c502b4374bf231890079574d001ff1
diasfulvio <dias.fulvio@gmail.com>
2015-10-01 18:53:24 -0300

    beta 4

--- Content/app.js
+++ Content/app.js
@@ -1,10 +1,13 @@
 ﻿var app = angular.module("app", ['ngRoute', 'ui.router']);
 
+//app.run(function (amMoment) {
+//    amMoment.changeLocale('pt-BR');
+//});
+
 app.directive('onlyNum', function () {
     return function (scope, element, attrs) {
         var keyCode = [8, 9, 13, 37, 39, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 110];
-        element.bind("keydown", function (event) {
-            //console.log($.inArray(event.which,keyCode));
+        element.bind("keydown", function (event) {            
             if ($.inArray(event.which, keyCode) == -1) {
                 scope.$apply(function () {
                     scope.$eval(attrs.onlyNum);
@@ -27,6 +30,14 @@ app.config(['$routeProvider',
               templateUrl: '/view/zipcode',
               controller: 'zipcode'
           })
+          .when('/cotacao', {
+              templateUrl: '/view/cotacao',
+              controller: 'cotacao'
+          })
+          .when('/address', {
+              templateUrl: '/view/address',
+              controller: 'address'
+          })
           .otherwise({ redirectTo: '/' });
   }]);
 app.controller('menu', function ($scope, $routeParams, $http) {
@@ -34,15 +45,72 @@ app.controller('menu', function ($scope, $routeParams, $http) {
 
 
 });
+app.controller('cotacao', function ($scope, $routeParams, $http) {
+
+    $scope.data = {};
+    $scope.clear = function()
+    {
+        $scope.data = {};
+    }
+    $scope.submit = function()
+    {
+        $http.post('/operation/cotacao', { })
+                .success(function (data) {
+                    data.dateBR = moment(data.Date).format('DD/MM/YYYY');
+                    $scope.data = data;
+                    console.log(data);
+
+                })
+                .error(function (data) {
+                    $scope.clear();
+                });
+    }
+    $scope.submit();
+
+});
+app.controller('address', function ($scope, $routeParams, $http) {
+    $scope.uf = '';
+    $scope.city = '';
+    $scope.address = '';
+    $scope.data = [];
+    $scope.message = '';
+
+    $scope.clear = function () {
+        $scope.uf = '';
+        $scope.city = '';
+        $scope.address = '';
+        $scope.data = [];
+        $scope.message = '';
+        $("#uf").focus();
+    }
+
+    $scope.submit = function ()
+    {
+        $http.post('/operation/address', { 'uf':$scope.uf, 'city':$scope.city, 'address': $scope.address })
+                .success(function (data) {
+                    if (parseInt(data.length) === 0)
+                    {
+                        $scope.message = 'Nenhum item encontrado';
+                    }
+                    $scope.data = data;
+                })
+                .error(function (data) {
+                    $scope.clear();
+                });
+    }
+
+});
 app.controller('zipcode', function ($scope, $routeParams, $http) {
 
     $scope.cep = '';
     $scope.data = {};
+    $scope.message = '';
 
     $scope.clear = function ()
     {
         $scope.cep = '';
         $scope.data = { 'Zip': "", 'Address': "", 'District': "", 'City': "", 'Uf': "", 'Ibge': "", 'Erro': true, 'Complement': "", 'Gia': "" };
+        $scope.message = '';
         $("#cep").focus();
     }
 
@@ -53,19 +121,18 @@ app.controller('zipcode', function ($scope, $routeParams, $http) {
                 .success(function (data) {                    
                     if (data.Erro) {
                         $scope.clear();
-                        alert('CEP inválido ou inexistente');
+                        $scope.message = 'CEP inválido ...';
                     } else {
                         $scope.data = data;
-                    }
-                    console.log(data);
+                        $scope.message = '';
+                    }                    
                 })
                 .error(function (data) {
-                    $scope.clear();
-                    console.log(data);
+                    $scope.clear();                    
                 });
         } else {
             $scope.clear();
-            alert("Cep inválido ...");
+            $scope.message = 'CEP inválido ...';
         }
         
     }
